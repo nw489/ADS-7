608a50b37787c46bd9ab1e5b858fd7ade738ea24
Letavin Egor <letavin123@yandex.ru>
2026-05-15 12:55:49 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,52 +1,62 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2022 NNTU-CS
 #include "train.h"
+#include <cstddef>
+
 
 Train::Train() : countOp(0), first(nullptr) {}
 
+Train::~Train() {
+  if (!first) return;
+  Car *cur = first->next;
+  while (cur != first) {
+    Car *tmp = cur->next;
+    delete cur;
+    cur = tmp;
+  }
+  delete first;
+}
+
 void Train::addCar(bool light) {
-  Car* newCar = new Car;
-  newCar->light = light;
-  newCar->next = nullptr;
-  newCar->prev = nullptr;
-  
-  if (first == nullptr) {
-    first = newCar;
-    first->next = first;
-    first->prev = first;
+  Car *car = new Car{light, nullptr, nullptr};
+  if (!first) {
+    car->next = car;
+    car->prev = car;
+    first = car;
   } else {
-    Car* last = first->prev;
-    last->next = newCar;
-    newCar->prev = last;
-    newCar->next = first;
-    first->prev = newCar;
+    Car *last = first->prev;
+    last->next = car;
+    car->prev = last;
+    car->next = first;
+    first->prev = car;
   }
 }
 
 int Train::getLength() {
-  if (first == nullptr) return 0;
-  
-  first->light = false;
-  int steps = 1;
-  
-  while (true) {
-    for (int i = 0; i < steps; i++) {
-      first = first->next;
-      countOp++;
-      if (!first->light) {
-        first->light = true;
-      }
-    }
-    
-    for (int i = 0; i < steps; i++) {
-      first = first->prev;
+  if (!first) return 0;
+  countOp = 0;
+
+  bool startLight = first->light; 
+  for (int k = 1; ; k++) {
+
+    Car *cur = first;
+    for (int i = 0; i < k; i++) {
+      cur = cur->next;
       countOp++;
     }
-    
-    if (!first->light) {
-      steps++;
-      first->light = false;
-    } else {
-      return steps;
+
+    cur->light = !cur->light;
+
+
+    for (int i = 0; i < k; i++) {
+      cur = cur->prev;
+      countOp++;
+    }
+
+
+
+    if (first->light != startLight) {
+      first->light = startLight; 
+      return k;
     }
   }
 }
