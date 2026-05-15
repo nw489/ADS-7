bb568a2e417c66d2bd9d9e558c5a6585bce0005a
Letavin Egor <letavin123@yandex.ru>
2026-05-15 13:32:28 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,15 @@
 // Copyright 2022 NNTU-CS
-#include "train.h"
 #include <iostream>
-#include <fstream>
-#include <random>
-
-Train createTrain(int n, int type) {
-  Train train;
-  std::random_device rd;
-  std::mt19937 gen(rd());
-  std::uniform_int_distribution<> dis(0, 1);
-  
-  for (int i = 0; i < n; i++) {
-    if (type == 0) train.addCar(false);
-    else if (type == 1) train.addCar(true);
-    else train.addCar(dis(gen));
-  }
-  return train;
-}
+#include "train.h"
 
 int main() {
-  std::ofstream file("data.csv");
-  file << "n,all_off,all_on,random\n";
-  
-  for (int n = 2; n <= 100; n++) {
-    long long off_sum = 0, on_sum = 0, rand_sum = 0;
-    
-    for (int trial = 0; trial < 10; trial++) {
-      Train t_off = createTrain(n, 0);
-      Train t_on = createTrain(n, 1);
-      Train t_rand = createTrain(n, 2);
-      
-      t_off.getLength();
-      t_on.getLength();
-      t_rand.getLength();
-      
-      off_sum += t_off.getOpCount();
-      on_sum += t_on.getOpCount();
-      rand_sum += t_rand.getOpCount();
-    }
-    
-    file << n << "," << off_sum/10.0 << "," << on_sum/10.0 << "," << rand_sum/10.0 << "\n";
-  }
-  
-  file.close();
+  Train train;
+  int count = 60; // кол-во вагонов
+
+  while (count--)
+    train.addCar(false);
+
+  std::cout << train.getLength() << std::endl;
+  std::cout << train.getOpCount() << std::endl;
   return 0;
 }
